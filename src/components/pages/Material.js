8cfb3fc50fc15b73240536450d9961728cc17b12
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-05-06 12:14:17 +0700

    frame project

---
+++ src/components/pages/Material.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Headers from '../wrapper/Headers';
+import Content from '../wrapper/Content';
+import Footer from '../wrapper/Footer';
+
+class Material extends React.Component {
+    render() {
+        return(
+            <div>
+                <Headers>...</Headers>
+                <Content>...</Content>
+                <Footer>...</Footer>
+            </div>
+        )
+    }
+}
+
+export default Material
