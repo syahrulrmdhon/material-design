2ab58969e2950db295d0e67b975ef3c746aa4193
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-05-10 13:26:24 +0700

    add react-favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Material from './components/pages/Material';
+import Favicon from 'react-favicon';
+
 
 const App = () => <div>
+    <Favicon url="http://oflisback.github.io/react-favicon/public/img/github.ico"/>
     <Route path='/' exact component={Material}/>
 </div>
 
--- src/components/pages/Material.js
+++ src/components/pages/Material.js
@@ -4,6 +4,9 @@ import Content from '../wrapper/Content';
 import Footer from '../wrapper/Footer';
 
 class Material extends React.Component {
+    componentDidMount(){
+        document.title = "Material Design"
+      }
     render() {
         return(
             <div>
