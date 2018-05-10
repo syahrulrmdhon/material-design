c04b6682b5b7e66835567dce32e5fc85facbc5a1
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-05-10 18:25:44 +0700

    change Rote path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Favicon from 'react-favicon';
 
 const App = () => <div>
     <Favicon url="https://raw.githubusercontent.com/syahrulrmdhon/material-design/master/public/favicon2.ico"/>
-    <Route path='/' exact component={Material}/>
+    <Route path={process.env.PUBLIC_URL + '/'} exact component={Material}/>
 </div>
 
 export default App;
